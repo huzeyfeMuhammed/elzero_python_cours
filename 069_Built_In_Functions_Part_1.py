2e0bfcbc369708117552af244fee3c91d241ee0c
huzi1993 <huzeyfe.for.is@gmail.com>
2024-10-25 08:13:27 +0300

    Any thing lek 3emmy

---
+++ 069_Built_In_Functions_Part_1.py
@@ -0,0 +1,30 @@
+# ------------------------
+# -- Built In Functions --
+# ------------------------
+# all()
+# any()
+# bin()
+# id()
+# ------------------------
+
+x = [1, 2, 3, 4, []]
+
+# if all(x):
+#     print ("All True")
+# else:
+#     print ("There at Least One False")
+
+# x = [ 0, 0, []]
+
+# if any (x):
+#     print ("There at Least One True")
+# else:
+#     print ("there is no any True element")
+
+
+# print (bin(100))
+
+a = 100
+b = 2
+print (id(a))
+print (id(b))
---
+++ 070_Built_In_Functions_Part_2.py
@@ -0,0 +1,24 @@
+# ------------------------
+# -- Built In Functions --
+# ------------------------
+# sum()
+# round()
+# range()
+# print()
+# ------------------------
+
+# sum(iterable, start)
+
+# a = [30, 15, 20, 3, 18, 40]
+
+# print (sum (a))
+# print (sum (a, 100))
+
+# round(number, numofdigits)
+
+print (round (10.5))
+print (round (105.551, 4))
+
+# range(start, end, step)
+
+print
